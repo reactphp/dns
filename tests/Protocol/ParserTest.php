62b913bbdef5813c30c1ae1913c2c39777f808b7
Christian Lück <christian@lueck.tv>
2018-06-26 12:40:35 +0200

    Add support for Message::TYPE_ANY and parse unknown types as binary data

--- tests/Protocol/ParserTest.php
+++ tests/Protocol/ParserTest.php
@@ -157,6 +157,31 @@ class ParserTest extends TestCase
         $this->assertSame('178.79.169.131', $response->answers[0]->data);
     }
 
+    public function testParseAnswerWithUnknownType()
+    {
+        $data = "";
+        $data .= "04 69 67 6f 72 02 69 6f 00";          // answer: igor.io
+        $data .= "23 28 00 01";                         // answer: type 9000, class IN
+        $data .= "00 01 51 80";                         // answer: ttl 86400
+        $data .= "00 05";                               // answer: rdlength 5
+        $data .= "68 65 6c 6c 6f";                      // answer: rdata "hello"
+
+        $data = $this->convertTcpDumpToBinary($data);
+
+        $response = new Message();
+        $response->header->set('anCount', 1);
+        $response->data = $data;
+
+        $this->parser->parseAnswer($response);
+
+        $this->assertCount(1, $response->answers);
+        $this->assertSame('igor.io', $response->answers[0]->name);
+        $this->assertSame(9000, $response->answers[0]->type);
+        $this->assertSame(Message::CLASS_IN, $response->answers[0]->class);
+        $this->assertSame(86400, $response->answers[0]->ttl);
+        $this->assertSame('hello', $response->answers[0]->data);
+    }
+
     public function testParseResponseWithCnameAndOffsetPointers()
     {
         $data = "";
