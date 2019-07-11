86e4ea8576c7d563bd2b912231a0623a7d78ee51
Christian Lück <christian@lueck.tv>
2019-07-11 11:30:49 +0200

    Makr all Record attributes as required, add documentation vs Query

--- src/Query/Query.php
+++ src/Query/Query.php
@@ -2,10 +2,30 @@
 
 namespace React\Dns\Query;
 
+/**
+ * This class represents a single question in a query/response message
+ *
+ * It uses a structure similar to `\React\Dns\Message\Record`, but does not
+ * contain fields for resulting TTL and resulting record data (IPs etc.).
+ *
+ * @link https://tools.ietf.org/html/rfc1035#section-4.1.2
+ * @see \React\Dns\Message\Record
+ */
 class Query
 {
+    /**
+     * @var string query name, i.e. hostname to look up
+     */
     public $name;
+
+    /**
+     * @var int query type (aka QTYPE), see Message::TYPE_* constants
+     */
     public $type;
+
+    /**
+     * @var int query class (aka QCLASS), see Message::CLASS_IN constant
+     */
     public $class;
 
     /**
